fba5bace5335389f545cef834c65f7174030a3e9
eidankhan <eidan.khan659@gmail.com>
2024-01-13 13:21:52 +0000

    configured h2 database

---
+++ src/main/java/com/projections/springdatajpaprojectionsexample/SpringDataJpaProjectionsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.projections.springdatajpaprojectionsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataJpaProjectionsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataJpaProjectionsExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projections/springdatajpaprojectionsexample/SpringDataJpaProjectionsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projections.springdatajpaprojectionsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaProjectionsExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
